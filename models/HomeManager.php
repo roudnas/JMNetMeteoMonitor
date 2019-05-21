7c3f593368c0d543dffd526b281863d7c299744b
MartmatiX <martin.malir@gmail.com>
2019-05-21 22:02:54 +0200

    Uprava souboru

--- models/HomeManager.php
+++ models/HomeManager.php
@@ -26,6 +26,17 @@
       return Db::singleQuery("select first 1 skip 0 absvlhkost, rosnybod from teplomer_data where idcidlo = $id order by id desc");
     }
 
+    public fuction getMinTemp($id){
+      Db::singleQuery("select min(teplota) from teplomer_data where datum like 'CURRENT_DATE, %' and id = $id group by id;");
+    }
+
+    public function getAvgTemp($id){
+      Db::singleQuery("select avg(teplota) from teplomer_data where datum like 'CURRENT_DATE, %' and id = $id group by id;");
+    }
+
+    public function getMaxTemp($id){
+      Db::singleQuery("select max(teplota) from teplomer_data where datum like 'CURRENT_DATE, %' and id = $id group by id;");
+    }
 
 
 
