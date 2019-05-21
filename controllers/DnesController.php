b5468f615b48858e950c9bde1074c9e47f5f797d
roudnas <dominikroudny@seznam.cz>
2019-05-21 19:08:48 +0200

    header

--- controllers/DnesController.php
+++ controllers/DnesController.php
@@ -7,6 +7,8 @@
     $this->data['prvniMer'] = $DnesManager->getPrvniMereni();
     $this->data['maxTeplota'] = $DnesManager->getExtremTeploty('max');
     $this->data['minTeplota'] = $DnesManager->getExtremTeploty('min');
+    $this->data['jmenoCidla'] = $DnesManager->getCidloNameById(3710);
+    $this->data['Vlhkost'] = $DnesManager->getCurrentVlhkost(3710);
     $this->pohled = 'uvod';
   }
   }
--- models/DnesManager.php
+++ models/DnesManager.php
@@ -18,8 +18,12 @@
       return Db::singleQuery("select datum, teplota from teplomer_data where teplota = (select $extrem(teplota) from teplomer_data)");
     }
 
-    public function getCidloNameById() {
-      
+    public function getCidloNameById($id) {
+      return Db::singleQuery("select jmeno from teplomer where id = $id");
+    }
+
+    public function getCurrentVlhkost($id) {
+      return Db::singleQuery("select first 1 skip 0 absvlhkost, rosnybod from teplomer_data where idcidlo = $id order by id desc");
     }
 
 
