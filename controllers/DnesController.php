7ca8ab447de774e3a9fa6c4c67560af87c5ba86d
roudnas <dominikroudny@seznam.cz>
2019-05-21 20:20:59 +0200

    smallmenu

--- controllers/DnesController.php
+++ controllers/DnesController.php
@@ -3,10 +3,10 @@
     public function zpracuj($params) {
     $DnesManager = new DnesManager();
     $this->data['teplota'] = $DnesManager->getCurrentTemp(3710);
-    $this->data['pocetMereni'] = $DnesManager->getPocetMereni();
-    $this->data['prvniMer'] = $DnesManager->getPrvniMereni();
-    $this->data['maxTeplota'] = $DnesManager->getExtremTeploty('max');
-    $this->data['minTeplota'] = $DnesManager->getExtremTeploty('min');
+    $this->data['pocetMereni'] = $DnesManager->getPocetMereni(3710);
+    $this->data['prvniMer'] = $DnesManager->getPrvniMereni(3710);
+    $this->data['maxTeplota'] = $DnesManager->getExtremTeploty('max',3710);
+    $this->data['minTeplota'] = $DnesManager->getExtremTeploty('min',3710);
     $this->data['jmenoCidla'] = $DnesManager->getCidloNameById(3710);
     $this->data['Vlhkost'] = $DnesManager->getCurrentVlhkost(3710);
     $this->pohled = 'uvod';
--- models/DnesManager.php
+++ models/DnesManager.php
@@ -1,21 +1,21 @@
 <?php
   class DnesManager {
 
-    public function getCurrentTemp($cidlo) {
-    return  Db::singleQueryNA("select teplota, substr(datum,9,10) || '.' ||substr(datum,7,7) || '.' || substr(datum,1,4) || ' ' || substr(datum,12,19)  from teplomer_data where idcidlo = $cidlo order by id desc;");
+    public function getCurrentTemp($id) {
+    return  Db::singleQueryNA("select teplota, substr(datum,9,10) || '.' ||substr(datum,7,7) || '.' || substr(datum,1,4) || ' ' || substr(datum,12,19)  from teplomer_data where idcidlo = $id order by id desc;");
     }
 
-    public function getPocetMereni() {
-      return Db::singleQueryNA("select count(*) from teplomer_data");
+    public function getPocetMereni($id) {
+      return Db::singleQueryNA("select count(*) from teplomer_data where idcidlo = $id");
     }
 
-    public function getPrvniMereni() {
-      return Db::singleQueryNA("select first 1 skip 0 substr(datum,9,10) || '.' ||substr(datum,7,7) || '.' || substr(datum,1,4)
-      from teplomer_data order by datum asc");
+    public function getPrvniMereni($id) {
+      return Db::singleQueryNA("select first 1 skip 0 substr(datum,9,10) || '.' ||substr(datum,6,7) || '.' || substr(datum,1,4)
+      from teplomer_data where idcidlo = $id order by datum asc");
     }
 
-    public function getExtremTeploty($extrem) {
-      return Db::singleQuery("select datum, teplota from teplomer_data where teplota = (select $extrem(teplota) from teplomer_data)");
+    public function getExtremTeploty($extrem, $id) {
+      return Db::singleQuery("select datum, teplota from teplomer_data where idcidlo = $id and teplota = (select $extrem(teplota) from teplomer_data where idcidlo = $id)");
     }
 
     public function getCidloNameById($id) {
