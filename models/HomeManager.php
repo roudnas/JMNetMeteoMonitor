9153452bb954c49d403a665cc4f827ef035e0107
MartmatiX <martin.malir@gmail.com>
2019-05-22 10:57:43 +0200

    Vymazani AktualneController

--- models/HomeManager.php
+++ models/HomeManager.php
@@ -27,15 +27,15 @@
     }
 
     public fuction getMinTemp($id){
-      Db::singleQuery("select min(teplota) from teplomer_data where datum like 'CURRENT_DATE, %' and id = $id group by id;");
+      return Db::singleQuery("select min(teplota) from teplomer_data where datum like 'CURRENT_DATE, %' and id = $id group by id;");
     }
 
     public function getAvgTemp($id){
-      Db::singleQuery("select avg(teplota) from teplomer_data where datum like 'CURRENT_DATE, %' and id = $id group by id;");
+      return Db::singleQuery("select avg(teplota) from teplomer_data where datum like 'CURRENT_DATE, %' and id = $id group by id;");
     }
 
     public function getMaxTemp($id){
-      Db::singleQuery("select max(teplota) from teplomer_data where datum like 'CURRENT_DATE, %' and id = $id group by id;");
+      return Db::singleQuery("select max(teplota) from teplomer_data where datum like 'CURRENT_DATE, %' and id = $id group by id;");
     }
 
 
